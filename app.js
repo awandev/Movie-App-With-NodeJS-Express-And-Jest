c07bd523f982838ecbe57149aed1b451889e37db
awalone <migoyeng@gmail.com>
2020-04-01 19:59:57 +0800

    add search page and movies page with style and google font

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 const express = require('express')
 const app = express();
+const path = require('path')
 
-app.get('/', (req, res) => {
-    res.send('<a href="/contact">Contact Us</a><br /><a href="/about">About Us</a>')
-});
 
-app.get('/contact', (req, res) => {
-    res.send('<h1>Contact Us Page</h1>')
-});
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
-app.get('/about', (req, res) => {
-    res.send('<h1>About Us Page</h1>')
+app.use(express.static('public'))
+
+app.get('/results', (req, res) => {
+    res.render('movies');
+})
+
+app.get('/search', (req, res) => {
+    res.render('search')
 })
 
 app.listen(3000, () => {
-    console.log('Server started on port 3000')
+    console.log('Server started at port 3000')
 })
