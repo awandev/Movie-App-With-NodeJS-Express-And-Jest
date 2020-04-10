9168b9d01aa1ca42a9e034943450643717662b71
awalone <migoyeng@gmail.com>
2020-04-10 13:59:50 +0800

    add search and results page on webscrapper movie

--- webscrapping/app.js
+++ webscrapping/app.js
@@ -1,2 +1,56 @@
 const express = require('express');
 const app = express();
+const path = require('path')
+const puppeteer = require('puppeteer')
+const cheerio = require('cheerio')
+
+let browser;
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine','ejs')
+app.use(express.static('public'))
+
+async function scrapeData(url, page) {
+    try {
+        await page.goto(url, {waitUntil: 'load', timeout: 0});
+        const html = await page.evaluate(() => document.body.innerHTML);
+        const $ = cheerio.load(html);
+
+        
+        return {
+            
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+async function getResults() {
+   
+}
+
+app.get('/results', async(req, res) => {
+    let url = req.query.search;
+
+    browser = await puppeteer.launch({
+        headless: false,
+        args: ['--no-sandbox'],
+        executablePath: '/usr/bin/google-chrome-stable'
+    });
+    const page = await browser.newPage();
+
+    let data = await scrapeData(url, page);
+    
+    browser.close()
+})
+
+app.get('/search', (req,res) => {
+    res.render('search')
+})
+
+
+
+app.listen(3000, () => {
+    console.log('Server started at port 3000')
+})
+
